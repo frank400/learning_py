20a962c1c82be71891df98c57a241e9583a131b1
frank004 <j.victorhuguenin2018@gmail.com>
2019-12-11 02:44:56 -0200

    Update HTML_v3.py

--- basics/Functions_Lesson/HTML_v3.py
+++ basics/Functions_Lesson/HTML_v3.py
@@ -2,7 +2,14 @@ def tag(text,classe='sucess', inline=False):
     tag='span' if inline else 'div'
     return f'<{tag} class="{classe}">{text}</{tag}>'
 
+def tag_list(*itens):
+    list=''.join((f'<li>{item}</li>' for item in itens))
+    return f'\n<ul>{list}</ul>'
+
+
+
 if __name__=="__main__":
     print(tag('block'))
     #or
     print(tag('block',inline=True))
+    print(tag_list('alice','frank','max','george'))
