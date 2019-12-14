4c87aba45eb1584ce739c977276b8e48b7c5fc5f
frank004 <j.victorhuguenin2018@gmail.com>
2019-12-14 04:38:46 -0200

    Update date.py

--- basics/Object_Oriented/date.py
+++ basics/Object_Oriented/date.py
@@ -1,9 +1,10 @@
 class Date:
+    def __init__(self,day,month,year):
+        self.day=day
+        self.month=month
+        self.year=year
     def __str__(self):
         return f'{self.day}/{self.month}/{self.year}'
 
-d1=Date()
-d1.day=15
-d1.month=12
-d1.year=2019
+d1=Date(15,12,2019)
 print(d1)
