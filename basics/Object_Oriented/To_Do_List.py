b2cacd280654b989e74f7fb75aa53b64791f33a1
frank004 <j.victorhuguenin2018@gmail.com>
2019-12-15 04:52:31 -0200

    Update To_Do_List.py

--- basics/Object_Oriented/To_Do_List.py
+++ basics/Object_Oriented/To_Do_List.py
@@ -1,14 +1,17 @@
-from datetime import datetime
+from datetime import datetime,timedelta
 class Project:
     def __init__(self,name):
         self.name=name
         self.list=[]
+
+    def __iter__(self):
+        return self.list.__iter__()
     
     def __str__(self):
         return f'{self.name} ({len(self.pendent())} undone task(s))'  
 
-    def add(self,desc):
-        self.list.append(Task(desc))
+    def add(self,desc,deadline=None):
+        self.list.append(Task(desc,deadline))
     
     def pendent(self):
         return [task for task in self.list if not task.done]
@@ -19,13 +22,23 @@ class Project:
 
 
 class Task:
-    def __init__(self,desc):
+    def __init__(self,desc,deadline):
         self.desc=desc
         self.done=False
         self.create=datetime.now()
+        self.deadline=deadline
     
     def __str__(self):
-        return self.desc + (' (Concluida)'if self.done else '')
+        status=[]
+        if self.done:
+            status.append('(Done)')
+        elif self.deadline:
+            if datetime.now()>self.deadline:
+                status.append("(expired)")
+            else:
+                days=(self.deadline -datetime.now()).days
+                status.append(f'(that task expire in {days} days)')
+        return f'{self.desc} ' + ''.join(status)
     
     def conclude(self):
         self.done = True
@@ -33,13 +46,13 @@ class Task:
 
 def main():
     home=Project('build a house')
-    home.add('build walls')
+    home.add('build walls',datetime.now()+ timedelta(days=4))
     home.add('make the floor')
     home.add('ceiling')
     print(home)
 
     home.search('make the floor').conclude()
-    for task in home.list:
+    for task in home:
         print(f'-{task}')
 
 
