078b40b0f6e38701507f06405aa5cbc28a8de214
frank400 <j.victorhuguenin2018@gmail.com>
2019-12-18 13:09:02 -0300

    A site class using operator overfload to add new features

--- basics/Object_Oriented/Operator_Overfload.py
+++ basics/Object_Oriented/Operator_Overfload.py
@@ -4,24 +4,27 @@ class Site:
         self.name=name
         self.desc=desc
         self.owner=owner
-        self.features=[]
+        self.features=[]  
     
-    def __str__(self):
-        return f'the name of this site is :{self.name} this is what it is :{self.desc} it owner is :{self.owner} and it features are :'
     
-    #def __iadd__(self,feature):
-        #self._add_feature(feature)
+    def __iadd__(self,feature):
+        self._add_feature(feature)
+        
+        return self
 
     def _add_feature(self,feature):
         self.features.append(Feature(feature))
     
     def searchfeature(self,name):
-        return [feature for feature in self.features if feature.name=='name'][-1]
+        return [feature for feature in self.features if feature.name==name][-1]
 
 class Feature():
     def __init__(self,name,desc=None):
         self.name=name
         self.desc=desc
+    
+    def __iter__(self):
+        return self.name
     
     def __str__(self):
         return f'the name of feature is {self.name} and it does this:{self.desc}'
@@ -31,9 +34,8 @@ class Feature():
     
 def main():
     waze=Site(name='Waze',desc='A app to smartfones based on GPS navegation to cars',owner='Google')
-    waze.features.append(Feature('GPS'))
+    waze += 'GPS'
     waze.searchfeature('GPS').add_description('track a path to vehicles by a global sistem of satellites')
-    print(waze)
     print(waze.searchfeature('GPS'))
 
 if __name__=='__main__':
