463449ce89b6ad82f1a1560df0dac8a66c58f7b4
frank400 <j.victorhuguenin2018@gmail.com>
2019-12-19 12:46:56 -0300

    Update main.py

--- basics/Object_Oriented_Challenge/main.py
+++ basics/Object_Oriented_Challenge/main.py
@@ -1,20 +1,22 @@
-from classes import Seller,Client,Purchase
+from classes import Seller, Client, Purchase
 from datetime import datetime
 
+
 def main():
-    client=Client('max',44)
-    seller=Seller('Frank',55,1024)
-    purchase_1=Purchase(client,datetime.now(),512)
-    purchase_2=Purchase(client,datetime(2019,4,9),256)
+    client = Client('max', 44)
+    seller = Seller('Frank', 55, 1024)
+    purchase_1 = Purchase(client, datetime.now(), 512)
+    purchase_2 = Purchase(client, datetime(2019, 4, 9), 256)
     client.regist_purchase(purchase_1)
     client.regist_purchase(purchase_2)
     print(client)
     print(seller)
 
-    total_values=client.get_total_purchase()
-    print(f'total value of purchases {total_values}$ in {len(client.purchases)} purchases')
+    total_values = client.get_total_purchase()
+    print(
+        f'total value of purchases {total_values}$ in {len(client.purchases)} purchases')
     print(f"last purchase date: {client.get_last_purchase()}")
 
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
