68047ca098fbb6d699abc48f1a406cdd59069c53
frank400 <j.victorhuguenin2018@gmail.com>
2019-12-20 22:43:34 -0300

    Update seller.py

--- basics/Object_Oriented_Challenge/classes/seller.py
+++ basics/Object_Oriented_Challenge/classes/seller.py
@@ -1,8 +1,10 @@
 from .person import Person
+
+
 class Seller(Person):
-    def __init__(self,name,age,payment):
-        super().__init__(name,age)
-        self.payment=payment
+    def __init__(self, name, age, payment):
+        super().__init__(name, age)
+        self.payment = payment
 
     def __str__(self):
         return f'Name:{self.name}\nAge:{self.age}\nPayment:{self.payment}'
