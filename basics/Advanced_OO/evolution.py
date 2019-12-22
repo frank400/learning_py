cb31aa8150da5531b409682feb6fbb2780d31733
frank400 <j.victorhuguenin2018@gmail.com>
2019-12-22 17:51:47 -0300

    Update evolution.py

--- basics/Advanced_OO/evolution.py
+++ basics/Advanced_OO/evolution.py
@@ -8,10 +8,27 @@ class Human:
         self.specie = 'homo neanderthalesis'
         return self
 
+    @staticmethod
+    def species():
+        adjetives = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapies')
+        return ('Australopithecus',) + tuple(f'Homo {adj}' for adj in adjetives)
+
+    @classmethod
+    def is_evolved(cls):
+        return cls.specie == cls.species()[-1]
+
+
+class Neaderthal(Human):
+    specie = Human.species()[-2]
+
+
+class HomoSapiens(Human):
+    specie = Human.species()[-1]
+
 
 if __name__ == '__main__':
-    joseph = Human('joseph')
-    gronk = Human('gronk').from_caves()
-
-    print(f'human.specie:{Human.specie}')
-    print(f'gronk.specie:{gronk.specie}')
+    joseph=HomoSapiens('Joseph')
+    gronk=Neaderthal('Gronk')
+    print(f'Evolution by the class:{",".join(HomoSapiens.species())}')
+    print(f'Evolution by the instance:{",".join(joseph.species())}')
+    print(f'is Joseph evolved? {joseph.is_evolved()}')
