0bb09445d2be911758bf27f47c1531b13af9c5fd
frank400 <j.victorhuguenin2018@gmail.com>
2019-12-31 00:13:48 -0300

    Update recursive_factorial.py

--- basics/Functional_programing/recursive_factorial.py
+++ basics/Functional_programing/recursive_factorial.py
@@ -1,7 +1,8 @@
 from functools import reduce
 
 
-def  factorial(n):
-    return n*(factorial(n-1) if (n-1)>1 else 1)
+def factorial(n):
+    return n*(factorial(n-1) if (n-1) > 1 else 1)
 
-print(factorial(3))
+
+print(factorial(10))
