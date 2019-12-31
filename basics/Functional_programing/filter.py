ec109e36ce596c51a84793da81ecf640c55a3bed
frank400 <j.victorhuguenin2018@gmail.com>
2019-12-30 23:17:29 -0300

    Update filter.py

--- basics/Functional_programing/filter.py
+++ basics/Functional_programing/filter.py
@@ -1,15 +1,17 @@
-persons=[
-    {'name':'joseph','age':18},
-    {'name':'max','age':16},
-    {'name':'frank','age':15},
-    {'name':'alexia','age':32}
+persons = [
+    {'name': 'joseph', 'age': 18},
+    {'name': 'max', 'age': 16},
+    {'name': 'frank', 'age': 15},
+    {'name': 'alexia', 'age': 32}
 ]
 
+
 def main():
-    legal_age=filter(lambda p:p['age']>18,persons)
+    legal_age = filter(lambda p: p['age'] > 18, persons)
     print(list(legal_age))
-    big_name=filter(lambda p:len(p['name'])>=5,persons)
+    big_name = filter(lambda p: len(p['name']) >= 5, persons)
     print(list(big_name))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
